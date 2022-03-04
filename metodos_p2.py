41d58117866d896ddcd37b20b8a22c5428450022
Yorbre25 <62673860+Yorbre25@users.noreply.github.com>
2022-03-04 11:26:29 -0600

    Commit Inicial

---
+++ metodos_p2.py
@@ -0,0 +1,9 @@
+def newton_H_m1(fun, x0, tol, iterMax):
+    print("hola")
+
+
+""" 
+    def newton_H_m2(fun, x0, tol, iterMax):
+    def newton_G_m1(fun, x0, tol, iterMax):
+    def newton_G_m2(fun, x0, tol, iterMax):
+"""
