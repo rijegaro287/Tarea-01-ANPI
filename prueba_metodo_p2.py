92db788facd0af643a06158035d5f1f2fb27809f
Yorbre25 <62673860+Yorbre25@users.noreply.github.com>
2022-03-05 10:13:12 -0600

    Parte 2.1 y 2.2 acabada

--- prueba_metodo_p2.py
+++ prueba_metodo_p2.py
@@ -3,14 +3,16 @@ from metodos_p2 import *
 f = "cos(x) - x"
 tol = 10**-5
 iterMax = 500
-#print("\n")
+x0 = 2.1
+
+
 print(f"Función selecionada (f1) : {f}")
 print("Método Newton:")
-newton_H_m1("(x - 1)**3 - 1", 2.1, tol, iterMax)
+newton_H_m1(f, x0, tol, iterMax)
 print("Método Kanwar-Tomar:")
-newton_H_m2("(x - 1)**3 - 1", 2.1, tol, iterMax)
+newton_H_m2(f, x0, tol, iterMax)
 print("Metodo Halley:")
-newton_G_m1("(x - 1)**3 - 1", 2.1, tol, iterMax)
+newton_G_m1(f, x0, tol, iterMax)
 print("Metodo Chebyshev:")
-newton_G_m2("(x - 1)**3 - 1", 2.1, tol, iterMax)
+newton_G_m2(f, x0, tol, iterMax)
 
